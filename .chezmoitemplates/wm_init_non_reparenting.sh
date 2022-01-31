5484a0bd0678c4e93b7c50c716dd9381d1a2a363
Adam Christiansen <hello@adamc.io>
2022-01-30 18:06:11 -0700

    feat: improve bspwm xfce integration

--- .chezmoitemplates/wm_init_non_reparenting.sh
+++
@@ -1,7 +0,0 @@
-# Initialize features for non-reparenting WMs.
-
-# The standard Java GUI tookit keeps a list of known non-reparenting window
-# managers. If using one that is not on that list, the windows might display a
-# blank screen. This can be solved by setting the name of the WM to one that is
-# known to be non-reparenting.
-wmname LG3D
