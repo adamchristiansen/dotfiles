60791113670650e39086293aecca0b842a4d51c4
Adam Christiansen <hello@adamc.io>
2022-03-08 15:41:39 -0700

    feat: add python notify util for macos

--- .chezmoitemplates/util.py
+++ .chezmoitemplates/util.py
@@ -1,5 +1,6 @@
 import collections
 import subprocess
+import shutil
 
 def clamp(v, lo, hi):
     """
@@ -63,34 +64,44 @@ def notify(
 
     (Run): Output of sending the notification.
     """
-    cmd = ["notify-send"]
-    if urgency is not None:
-        cmd.extend(["--urgency", urgency])
-    if app is not None:
-        cmd.extend(["--app-name", app])
-    if icon is not None:
-        cmd.extend(["--icon", icon])
-    if tag is not None:
-        cmd.extend(["--hint", f"string:x-dunst-stack-tag:{tag}"])
-    if progress is not None:
-        progress = clamp(progress, 0, 100)
-        cmd.extend(["--hint", f"int:value:{progress}"])
-    if hints is not None:
-        for k, v in hints.items():
-            if isinstance(v, int):
-                cmd.extend(["--hint", f"int:{k}:{v}"])
-            elif isinstance(v, float):
-                cmd.extend(["--hint", f"float:{k}:{v}"])
-            elif isinstance(v, str):
-                cmd.extend(["--hint", f"string:{k}:{v}"])
-            else:
-                raise TypeError("hints must be int, float, or string")
-    cmd.append("--")
-    if summary is not None:
-        cmd.append(summary)
+    if shutil.which("osascript") is not None:
+        cmd = ["osascript", "-e"]
+        title = app if app is not None else "Notification"
+        message = body
+        text = "display notification"
+        if body is not None:
+            text += f' "{message}"'
+        if app is not None:
+            text += f' with title "{app}"'
+        cmd.append(text)
     else:
-        cmd.append(" ")
-    if body is not None:
-        cmd.append(body)
-
+        cmd = ["notify-send"]
+        if urgency is not None:
+            cmd.extend(["--urgency", urgency])
+        if app is not None:
+            cmd.extend(["--app-name", app])
+        if icon is not None:
+            cmd.extend(["--icon", icon])
+        if tag is not None:
+            cmd.extend(["--hint", f"string:x-dunst-stack-tag:{tag}"])
+        if progress is not None:
+            progress = clamp(progress, 0, 100)
+            cmd.extend(["--hint", f"int:value:{progress}"])
+        if hints is not None:
+            for k, v in hints.items():
+                if isinstance(v, int):
+                    cmd.extend(["--hint", f"int:{k}:{v}"])
+                elif isinstance(v, float):
+                    cmd.extend(["--hint", f"float:{k}:{v}"])
+                elif isinstance(v, str):
+                    cmd.extend(["--hint", f"string:{k}:{v}"])
+                else:
+                    raise TypeError("hints must be int, float, or string")
+        cmd.append("--")
+        if summary is not None:
+            cmd.append(summary)
+        else:
+            cmd.append(" ")
+        if body is not None:
+            cmd.append(body)
     return run(cmd)
