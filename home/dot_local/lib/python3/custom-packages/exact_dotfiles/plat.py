e667be7191683b36ac5f1a398578222bfa1afa36
Adam Christiansen <hello@adamc.io>
2022-09-18 18:39:54 -0600

    feat: add ssh-magic

--- home/dot_local/lib/python3/custom-packages/exact_dotfiles/plat.py
+++ home/dot_local/lib/python3/custom-packages/exact_dotfiles/plat.py
@@ -1,9 +1,11 @@
+import errno
 import os
 import shutil
 
 class plat:
   """Platform utilities."""
 
+  # TODO Rename has_cmd
   @staticmethod
   def hascmd(*commands):
     """Check that all commands exist in PATH"""
@@ -14,6 +16,7 @@ class plat:
         return False
     return True
 
+  # TODO Rename has_env
   @staticmethod
   def hasenv(*variables):
     """Check that all environment variables are defined."""
@@ -22,4 +25,15 @@ class plat:
     for variable in variables:
       if os.getenv(variable) is None:
         return False
+    return True
+
+  @staticmethod
+  def pid_exists(pid):
+    """Check that pid exists."""
+    if pid == 0:
+      return True
+    try:
+      os.kill(pid, 0)
+    except OSError as err:
+      return err.errno == errno.EPERM
     return True
--- home/dot_local/lib/python3/custom-packages/exact_dotfiles/run.py
+++ home/dot_local/lib/python3/custom-packages/exact_dotfiles/run.py
@@ -1,20 +1,26 @@
 import collections
+import os
 import subprocess
 
 RunResult = collections.namedtuple("Run", ["exitcode", "stdout", "stderr"])
 
-def run(cmd, input=None, check=False):
+def run(cmd, input=None, check=False, append_env=None):
   """
   Run a command specified as a list of strings. The optional input is used for
-  stdin. An exception is raised on error when check is set. Returns the
-  exitcode, stdout, and stderr of the command.
+  stdin. Environment variables can be added with append environment option. An
+  exception is raised on error when check is set. Returns the exitcode, stdout,
+  and stderr of the command.
   """
+  env = None
+  if append_env is not None and append_env:
+    env = {**os.environ, **append_env}
   p = subprocess.run(
     cmd,
     input=input,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
-    check=check)
+    check=check,
+    env=env)
   return RunResult(
     p.returncode,
     (p.stdout if p.stdout is not None else b"").decode("utf-8").strip(),
