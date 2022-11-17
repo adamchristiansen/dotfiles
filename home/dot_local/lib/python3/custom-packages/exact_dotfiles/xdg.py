848034f6e2196b531ae1d9a519c1d0c5397b3dfe
Adam Christiansen <hello@adamc.io>
2022-11-16 18:22:20 -0700

    feat: fix dotfiles xdg module

--- home/dot_local/lib/python3/custom-packages/exact_dotfiles/xdg.py
+++ home/dot_local/lib/python3/custom-packages/exact_dotfiles/xdg.py
@@ -26,32 +26,32 @@ def state_home(*paths):
 
 def desktop_dir(*paths):
   """Get the XDG desktop directory."""
-  return _get_join("XDG_DESKTOP_HOME", "Desktop", *paths)
+  return _get_join("XDG_DESKTOP_DIR", "Desktop", *paths)
 
 def documents_dir(*paths):
   """Get the XDG documents directory."""
-  return _get_join("XDG_DOCUMENTS_HOME", "Documents", *paths)
+  return _get_join("XDG_DOCUMENTS_DIR", "Documents", *paths)
 
 def download_dir(*paths):
   """Get the XDG download directory."""
-  return _get_join("XDG_DOWNLOAD_HOME", "Downloads", *paths)
+  return _get_join("XDG_DOWNLOAD_DIR", "Downloads", *paths)
 
 def music_dir(*paths):
   """Get the XDG music directory."""
-  return _get_join("XDG_MUSIC_HOME", "Music", *paths)
+  return _get_join("XDG_MUSIC_DIR", "Music", *paths)
 
 def pictures_dir(*paths):
   """Get the XDG pictures directory."""
-  return _get_join("XDG_PICTURES_HOME", "Pictures", *paths)
+  return _get_join("XDG_PICTURES_DIR", "Pictures", *paths)
 
 def publicshare_dir(*paths):
   """Get the XDG public share directory."""
-  return _get_join("XDG_PUBLICSHARE_HOME", "Public", *paths)
+  return _get_join("XDG_PUBLICSHARE_DIR", "Public", *paths)
 
 def templates_dir(*paths):
   """Get the XDG templates directory."""
-  return _get_join("XDG_TEMPLATES_HOME", "Templates", *paths)
+  return _get_join("XDG_TEMPLATES_DIR", "Templates", *paths)
 
 def videos_dir(*paths):
   """Get the XDG videos directory."""
-  return _get_join("XDG_VIDEOS_HOME", "Videos", *paths)
+  return _get_join("XDG_VIDEOS_DIR", "Videos", *paths)
